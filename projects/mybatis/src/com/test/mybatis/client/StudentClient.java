2e19b533df72aa5e2546244edc38b134d40c9142
QJF <1677861817@qq.com>
2014-11-22 18:47:03 +0800

    查询实例。

--- projects/mybatis/src/com/test/mybatis/client/StudentClient.java
+++ projects/mybatis/src/com/test/mybatis/client/StudentClient.java
@@ -1,5 +1,10 @@
 package com.test.mybatis.client;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.test.mybatis.inter.IStudentInterface;
@@ -9,7 +14,8 @@ import com.test.mybatis.utils.MyBatisUtils;
 public class StudentClient {
 
 	public static void main(String[] args) {
-		typeTwo();
+//		typeTwo();
+		getStudentByNo();
 	}
 	
 	//
@@ -24,13 +30,105 @@ public class StudentClient {
 	}
 	
 	/**
-	 * 以接口方式调用
+	 * 以接口方式调用---根据id查找单个结果
+	 * 以#方式传参
+	 * 输出的sql为select * from student where student_id = ?
 	 */
 	public static void typeTwo() {
 		SqlSession session = MyBatisUtils.getSqlSessionFactory().openSession();
 		IStudentInterface stuInter = session.getMapper(IStudentInterface.class);
 		Student student = stuInter.selectStudentByID(1);
 		System.out.println(student.getStudentNo());
+		session.close();
+	}
+	
+	/**
+	 * 以$方式传参
+	 * 输出的sql为select * from student where student_id = 1
+	 */
+	public static void getSudentById() {
+		SqlSession session = MyBatisUtils.getSqlSessionFactory().openSession();
+		IStudentInterface stuInter = session.getMapper(IStudentInterface.class);
+		Student student = stuInter.getStudentById(1);
+		System.out.println(student.getStudentNo());
+		session.close();
+	}
+	
+	/**
+	 * 根据学号获取信息
+	 * #方式传参
+	 * 输出sql:select * from student where student_no = ?
+	 */
+	public static void getStudentByNo() {
+		SqlSession session = MyBatisUtils.getSqlSessionFactory().openSession();
+		IStudentInterface stuInter = session.getMapper(IStudentInterface.class);
+		Student student = stuInter.getStudentByNo("abc");
+		System.out.println(student.getStudentNo());
+		session.close();
+	}
+	
+	/**
+	 * $方式传参
+	 * 输出sql:select * from student where student_no = 'abc' 
+	 * 
+	 * 如果实参的值为  abc,则输出select * from student where student_no = abc，报错 
+	 */
+	public static void getStudentByNo1() {
+		SqlSession session = MyBatisUtils.getSqlSessionFactory().openSession();
+		IStudentInterface stuInter = session.getMapper(IStudentInterface.class);
+		Student student = stuInter.getStudentByNo1("'abc'");
+		System.out.println(student.getStudentNo());
+		session.close();
+	}
+	
+	/**
+	 * 获取结果集---array
+	 */
+	public static void getStudents() {
+		SqlSession session = MyBatisUtils.getSqlSessionFactory().openSession();
+		IStudentInterface stuInter = session.getMapper(IStudentInterface.class);
+		int[] ids = {1,2,3,4};
+		List<Student> list = (List<Student>)stuInter.getStudents(ids);
+		for(Student stu : list) {
+			System.out.println(stu.getStudentNo());
+		}
+		session.close();
+	}
+	
+	/**
+	 * 获取结果集---list
+	 */
+	public static void getStudents1() {
+		SqlSession session = MyBatisUtils.getSqlSessionFactory().openSession();
+		IStudentInterface stuInter = session.getMapper(IStudentInterface.class);
+		List<String> ids = new ArrayList<String>();
+		ids.add("1");
+		ids.add("2");
+		ids.add("3");
+		List<Student> list = (List<Student>)stuInter.getStudents1(ids);
+		for(Student stu : list) {
+			System.out.println(stu.getStudentNo());
+		}
+		session.close();
+	}
+	
+	/**
+	 * 获取结果集----map
+	 */
+	public static void getStudents2() {
+		SqlSession session = MyBatisUtils.getSqlSessionFactory().openSession();
+		IStudentInterface stuInter = session.getMapper(IStudentInterface.class);
+		List<String> ids = new ArrayList<String>();
+		ids.add("1");
+		ids.add("2");
+		ids.add("3");
+		
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
+		map.put("ids", ids);
+		List<Student> list = (List<Student>)stuInter.getStudents2(map);
+		for(Student stu : list) {
+			System.out.println(stu.getStudentNo());
+		}
 		session.close();
 	}
 }
--- projects/mybatis/src/com/test/mybatis/inter/IStudentInterface.java
+++ projects/mybatis/src/com/test/mybatis/inter/IStudentInterface.java
@@ -1,13 +1,63 @@
 package com.test.mybatis.inter;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.test.mybatis.model.Student;
 
 public interface IStudentInterface {
 
 	/**
 	 * 方法名与Student.xml中的查询语句的id相同
+	 * #方式传入参数
 	 * @param id
 	 * @return
 	 */
 	public Student selectStudentByID(int id);
+	
+	/**
+	 * 方法名与Student.xml中的查询语句的id相同
+	 * $方式传入参数
+	 * @param id
+	 * @return
+	 */
+	public Student getStudentById(@Param("id")int id);
+	
+	/**
+	 * 根据studentNo查找
+	 * #方式传参
+	 * @param no
+	 * @return
+	 */
+	public Student getStudentByNo(String no);
+	
+	/**
+	 * 根据studentNo查找
+	 * $方式传参
+	 * @param no
+	 * @return
+	 */
+	public Student getStudentByNo1(@Param("no")String no);
+	
+	/**
+	 * 得到结果集----array
+	 * @return
+	 */
+	public List<Student> getStudents(int[] ids);
+	
+	/**
+	 * 得到结果集----list
+	 * @param list
+	 * @return
+	 */
+	public List<Student> getStudents1(List<String> list);
+	
+	/**
+	 * 获取结果集----map
+	 * @param map
+	 * @return
+	 */
+	public List<Student> getStudents2(Map<String,List<String>> map);
 }
